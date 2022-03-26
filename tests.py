4d86499b2a69c8d334876f19f67b422627b485fa
Anastasia <anastasia@MacBook-Air-macbook.local>
2022-03-26 22:46:01 +0300

    test

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get_hello_endpoint(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, b'Hello lolkek!')
+        self.assertEqual(r.data, b'Hello lolkek')
 
     def test_post_hello_endpoint(self):
         r = self.app.post('/')
